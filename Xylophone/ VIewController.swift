cb4ada21a614f55b248b73edf9acc99af69be387
Abhishek <abhishek.marriala@gmail.com>
2020-03-23 14:22:28 +0530

    Add files via upload

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Abhishek Marriala on 23/03/20.
+//  Copyright © 2020 Abhishek Marriala. All rights reserved.
+//
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var selectedSoundFileName = ""
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        print(selectedSoundFileName)
+        
+        playSound()
+        
+    }
+    
+    func playSound() {
+        
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        audioPlayer = try! AVAudioPlayer(contentsOf: soundURL!)
+        
+        audioPlayer.play()
+    }
+    
+  
+
+}
+
